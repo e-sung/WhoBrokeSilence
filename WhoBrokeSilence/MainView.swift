0260c6d4ec40643ae60f10550ff3195d3137267f
Sungdoo Yoo <dev.esung@gmail.com>
2020-03-16 20:49:13 +0900

    feat: first mvp

--- WhoBrokeSilence/MainView.swift
+++ WhoBrokeSilence/MainView.swift
@@ -8,10 +8,16 @@
 
 import SwiftUI
 import SoundRecognizer
+import Combine
 
 class MainViewModel: ObservableObject {
     let soundRecognizer = SoundeRecornizer()
     @Published var currentSoundLevel: Float = 0
+    @Published var noiseCount:Int = 0
+    @Published var noiseCircleRadius:CGFloat = 150
+    @Published var dragCircleColor:Color = .green
+    var cancelBag:[Cancellable] = []
+    
     init() {
         soundRecognizer.audioLevelHandler = { level in
             DispatchQueue.main.async {
@@ -19,7 +25,35 @@ class MainViewModel: ObservableObject {
             }
         }
         soundRecognizer.startListening()
+        
+        let soundLevelToCircleRadiusBinding =
+            $currentSoundLevel
+                .map { CGFloat($0 )}
+                .map { $0 * UIScreen.main.bounds.width * 3}
+                .assign(to: \.noiseCircleRadius, on: self)
+        cancelBag.append(soundLevelToCircleRadiusBinding)
+        
+        let circleRadiusToColorBinding = $noiseCircleRadius
+            .map { radius -> Color in
+                let draggableCircleRadius = DragCircleViewModel.shared.dragCircleRadius
+                if radius > draggableCircleRadius {
+                    return .red
+                }
+                return .green
+            }
+        .assign(to: \.dragCircleColor, on: self)
+        
+        cancelBag.append(circleRadiusToColorBinding)
+
     }
+    
+    private func resetBsPerMinuteEveryMinute() {
+        let timer = Timer.scheduledTimer(withTimeInterval: 60, repeats: true, block: { timer in
+            self.noiseCount = 0
+        })
+        timer.fire()
+    }
+    
 }
 
 
@@ -27,35 +61,19 @@ struct MainView: View {
     @ObservedObject var viewModel = MainViewModel()
     @ObservedObject var dragCircleViewModel = DragCircleViewModel.shared
     let maxSize = UIScreen.main.bounds.size.width
-    var noiseLevel: CGFloat {
-        CGFloat(viewModel.currentSoundLevel)
-    }
-    var noiseCircleRadius:CGFloat {
-        noiseLevel * UIScreen.main.bounds.width * 3
-    }
     
     let dragCircle = DraggableCircle()
-    
-    var dragCircleColor: Color {
-        if dragCircleViewModel.dragCircleRadius > noiseCircleRadius {
-            print(dragCircleViewModel.dragCircleRadius)
-            return .green
-        }
-        else {
-            return .red
-        }
-    }
 
     var body: some View {
         ZStack {
-            Circle().frame(width: self.noiseCircleRadius,
-                           height: self.noiseCircleRadius,
+            Circle().frame(width: self.viewModel.noiseCircleRadius,
+                           height: self.viewModel.noiseCircleRadius,
                            alignment: .center)
                 .foregroundColor(.gray)
                 .animation(Animation.easeInOut)
             
             self.dragCircle
-                .foregroundColor(self.dragCircleColor)
+                .foregroundColor(self.viewModel.dragCircleColor)
         }
     }
 }
